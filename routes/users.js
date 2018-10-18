551e3bac330b05224aa7b861e72f86592b221c4b
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-10-18 10:34:53 -0300

    Excluindo arquivos da API

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/users');
-
-/* GET users listing. */
-router.post('/', controller.create);
-
-module.exports = router;
