387aff83ae68a43d512840f3a7f00245aa15b8d8
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-10-22 12:12:03 -0300

    Exportando classes

---
+++ index.js
@@ -0,0 +1,7 @@
+const Scope = require('./lib/Scope');
+const InvalidParam = require('./lib/InvalidParam');
+
+module.exports = {
+    Scope: Scope.default,
+    InvalidParam: InvalidParam.default,
+};
